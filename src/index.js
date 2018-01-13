fb4faa0d79f8d2b9c56a586a000b49eea5087d88
Vibhanshu <vibhanshu86@gmail.com>
2018-01-13 21:52:56 +0530

    adds assets, prop-types and enables HMR

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
+import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+render(<App />, rootEl);
+
+if (module.hot) {
+  console.log('hot module enabled');
+  module.hot.accept('./App', function() {
+    const NextApp = require('./App').default;
+    render(<NextApp/>, rootEl);
+  });
+}
+
 registerServiceWorker();
