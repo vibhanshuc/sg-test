82116b9357c06bf039c1c1793d6fad7391207b07
Vibhanshu <vibhanshu86@gmail.com>
2018-01-15 10:14:34 +0530

    adds smooth scroll polyfill

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import SmoothScroll from 'smoothscroll-polyfill';
 import './index.css';
 import App from './App';
-
 import registerServiceWorker from './registerServiceWorker';
+
+SmoothScroll.polyfill();
 
 /* global document */
 
